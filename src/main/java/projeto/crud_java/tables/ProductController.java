c24212cd264d8e1a83cd3aa41135c93e60ecf595
Leonardo Bonardi <LBonardi2312@gmail.com>
2024-06-16 19:29:27 -0300

    Commit da ProductController

--- src/main/java/projeto/crud_java/tables/ProductController.java
+++ src/main/java/projeto/crud_java/tables/ProductController.java
@@ -8,39 +8,85 @@ import projeto.crud_java.dbConection.DataBaseUtility;
 public class ProductController {
     //TODO: Implementar os métodos de atualizar, consultar, cadastrar e remover produtos do BD
 
-    public void insert(Product product) throws Exception {
+    public static void insert(Product product) throws Exception {
         String pName = product.getName();
         double price = product.getListPrice();
         StringBuilder col = new StringBuilder("(");
         StringBuilder lin = new StringBuilder("(");
 
         col.append("productName");
-        lin.append("'" + pName + "'");
+        lin.append("\'" + pName + "\'");
 
-        col.append(", shorDescription");
-        if(!(product.getShortDescription().equals("None") || product.getShortDescription().equals(null)))
-            lin.append(", '" + product.getShortDescription() + "'");
+        if (!(product.getShortDescription().equals("None") || product.getShortDescription().equals(null))) {
+            col.append(", shortDescription");
+            lin.append(", '" + product.getShortDescription() + "\'");
+        }
 
-        col.append(", brand");
-        if(!(product.getBrand().equals("Unknow") || product.getBrand().equals(null)))
-            lin.append(", '" + product.getBrand() + "'");
+        if (!(product.getBrand().equals("Unknow") || product.getBrand().equals(null))) {
+            col.append(", brand");
+            lin.append(", '" + product.getBrand() + "\'");
+        }
 
-        col.append("category");
-        if(!(product.getBrand().equals("Unknow") || product.getBrand().equals(null)))
-            lin.append(", '" + product.getBrand() + "'");
+        if (!(product.getCategory().equals("Unknow") || product.getCategory().equals(null))) {
+            col.append(", category");
+            lin.append(", '" + product.getBrand() + "\'");
+        }
 
-        col.append("listPrice");
-        lin.append(", '" + price + "'");
+        col.append(", listPrice");
+        lin.append(", '" + price + "\'");
 
-        col.append("cost");
-        if(!(product.getCost() == -1.0))
-            lin.append(", '" + product.getBrand() + "'");
+        if (!(product.getCost() == -1.0)) {
+            col.append("cost");
+            lin.append(", '" + product.getBrand() + "\'");
+        }
 
         col.append(")");
         lin.append(")");
 
-        String sql = "Colunas " + col.toString() + "Linhas " + lin.toString();
+        DataBaseUtility.insert(col.toString(), lin.toString());
+    }
 
-        DataBaseUtility.insert(sql);
+    public static void update(Product product) throws Exception {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append("productName = " + "\"" + product.getName() + "\"");
+
+        if (!(product.getShortDescription().equals("None") || product.getShortDescription().equals(null))) {
+            sql.append(", shortDescription = " + "\"" + product.getShortDescription() + "\"");
+        }
+
+        if (!(product.getBrand().equals("Unknow") || product.getBrand().equals(null))) {
+            sql.append(", brand = " + "\"" + product.getBrand() + "\"");
+        }
+
+        if (!(product.getCategory().equals("Unknow") || product.getCategory().equals(null))) {
+            sql.append(", category = " + "\"" + product.getCategory() + "\"");
+        }
+
+        sql.append(", listPrice = " + "\"" + product.getListPrice() + "\"");
+
+
+        if (!(product.getCost() == -1.0)) {
+            sql.append(", cost = " + "\"" + product.getCost() + "\"");
+        }
+
+        DataBaseUtility.update(sql, product.getName());
+
+    }
+
+    public static Product consult(String pName) {
+        Product product = DataBaseUtility.getProduct(pName); // Obtém o produto uma vez
+
+        if (product == null) {
+            throw new NullPointerException("Product can't be null");
+        }
+        return product;
+    }
+
+    public static void delete(String pName)throws Exception{
+        if(DataBaseUtility.getProduct(pName) == null){
+            throw new NullPointerException("Product is null. It must be something");
+        }
+        DataBaseUtility.delete(pName);
     }
 }
